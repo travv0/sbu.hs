e28b4be54e32fcacadddfc14270c8371517419ce
Travis <tssund93@gmail.com>
2018-12-14 02:35:32 -0600

    working on command line options

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,21 +1,5 @@
 module Lib
-  ( someFunc
-  )
+  ()
 where
 
-data Command
-  = Backup BackupOptions
-  -- | Add AddOptions
-  -- | List ListOptions
-  -- | Info InfoOptions
-  -- | Remove RemoveOptions
-  -- | Edit EditOptions
-  -- | Config ConfigOptions
-
-data BackupOptions = BackupOptions
-  { games :: [String]
-  , loop :: Bool
-  }
-
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+dummy = undefined
